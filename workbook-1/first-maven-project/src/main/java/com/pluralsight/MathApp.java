c2b4873c01867c4749c30626d07f03bd0f11d51c
Noel Guillen <noelblas274@gmail.com>
2024-04-10 11:50:05 -0400

    excerise for class on Math function

--- workbook-1/first-maven-project/src/main/java/com/pluralsight/MathApp.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/MathApp.java
@@ -2,6 +2,33 @@ package com.pluralsight;
 
 public class MathApp {
     public static void main(String[] args) {
+        // first question
+        int bobSalary = 2000;
+        int garySalary = 5500;
+        int highestSalary = Math.max(bobSalary, garySalary);
+        System.out.println("The highest salary is ..." + highestSalary);
+        //second question
+        int carPrice = 30_000;
+        int truckPrice = 55_000;
+        int cheapestVehicle = Math.min(carPrice, truckPrice);
+        System.out.println("The cheapest car is ..." + cheapestVehicle);
+        // third question
+        double area = (7.25 * 7.25) * Math.PI;
+        System.out.println(String.format("%.2f", area));
+        // forth question
+        double squareRoot = Math.sqrt(5.0);
+        System.out.println(String.format("%.2f", squareRoot));
+        // fifth question
+        double distance = Math.sqrt(Math.pow((85 - 5), 2) + Math.pow((50 - 10), 2));
+        System.out.println(String.format("%.2f", distance));
+        // sixth question
+        double absoluteValue = Math.abs(-3.8);
+        System.out.println(absoluteValue);
+        // seventh question
+        int randomNumber = (int)(Math.random() * 2);
+        System.out.println(randomNumber);
+
+
 
     }
 }
