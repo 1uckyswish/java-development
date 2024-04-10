771740ae422e6fd21bf82604f6b89b08fa0c8bf4
Noel Guillen <noelblas274@gmail.com>
2024-04-10 15:18:14 -0400

    Updated Payroll Calculator with overtime pay

--- workbook-1/first-maven-project/src/main/java/com/pluralsight/PayrollCalculator.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,2 +1,30 @@
-package com.pluralsight;public class PayrollCalculator {
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Hello and welcome!");
+        System.out.print("What is your name Fellow Traveler? ");
+        String userName = scan.nextLine();
+        System.out.print("How many hours have you worked? ");
+        double hours = scan.nextDouble();
+        System.out.print("Alright and how much would you like to get paid for your work " + userName + "? ");
+        double hourlyPay = scan.nextDouble();
+        scan.nextLine();
+        double grossPay = (hours * hourlyPay);
+
+        if(hours > 40){
+            int amountOfOvertime = (int) hours - 40;
+            double overtimePay = hourlyPay * 1.5;
+            double overtimeEarnings = overtimePay * amountOfOvertime;
+            grossPay += overtimeEarnings;
+            System.out.printf("Great job, %s! You've earned $%.2f this week, with %d hours of overtime.\n", userName, grossPay, amountOfOvertime);
+        }else{
+            System.out.printf("Impressive work, %s! Your earnings for this week are $%.2f.\n", userName, grossPay);
+        }
+        System.out.printf("Congratulations, %s! You've worked %.2f hours this week! Your commitment is inspiring! :)\n", userName, hours);
+        scan.close();
+    }
 }
