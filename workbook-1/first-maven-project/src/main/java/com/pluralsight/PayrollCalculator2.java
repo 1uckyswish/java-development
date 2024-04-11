b5446261daff5947011dd48d5675f3f84bbb1198
Noel Guillen <noelblas274@gmail.com>
2024-04-11 10:08:17 -0400

    refactored version of calculator

--- workbook-1/first-maven-project/src/main/java/com/pluralsight/PayrollCalculator2.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/PayrollCalculator2.java
@@ -4,33 +4,64 @@ import java.util.Scanner;
 
 public class PayrollCalculator2 {
     static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Hello and welcome!");
-        System.out.print("What is your name Fellow Traveler? ");
-        String userName = "";
-        System.out.print("How many hours have you worked? ");
-        double hours = scan.nextDouble();
-        System.out.print("Alright and how much would you like to get paid for your work " + userName + "? ");
-        double hourlyPay = scan.nextDouble();
-        scan.nextLine();
-        double grossPay = (hours * hourlyPay);
+    static String userName = "";
+    static double hours;
+    static double hourlyPay;
+    static double grossPay;
 
-        if(hours > 40){
-            int amountOfOvertime = (int) hours - 40;
-            double overtimePay = hourlyPay * 1.5;
-            double overtimeEarnings = overtimePay * amountOfOvertime;
-            grossPay += overtimeEarnings;
-            System.out.printf("Great job, %s! You've earned $%.2f this week, with %d hours of overtime.\n", userName, grossPay, amountOfOvertime);
-        }else{
-            System.out.printf("Impressive work, %s! Your earnings for this week are $%.2f.\n", userName, grossPay);
+    public static void main(String[] args) {
+        // Call the promptUser method to get user input
+        promptUser();
+
+        // Check if the user worked overtime
+        if (hours > 40) {
+            displayOverTime(userName, hours, hourlyPay);
+            payBreakDown();
+        } else {
+            displayMessage(hours, hourlyPay);
+            payBreakDown();
         }
-        System.out.printf("Congratulations, %s! You've worked %.2f hours this week! Your commitment is inspiring! :)\n", userName, hours);
         scan.close();
     }
 
-//    public static void getUserInput(String userName, String){
-//        userName =  scan.nextLine();
-//    }
+    public static void displayOverTime(String userName, double hours, double hourlyPay) {
+        int amountOfOvertime = (int) hours - 40;
+        double overtimePay = hourlyPay * 1.5;
+        double overtimeEarnings = overtimePay * amountOfOvertime;
+        double grossPayBeforeOvertime = 40 * hourlyPay;
+        grossPayBeforeOvertime += overtimeEarnings;
+        grossPay = grossPayBeforeOvertime;
+        System.out.printf("Great job, %s! You've earned $%.2f this week, with %d hours of overtime.\n", userName,
+                grossPayBeforeOvertime, amountOfOvertime);
+        System.out.printf("Congratulations, %s! You've worked %.2f hours this week! Your commitment is inspiring! :)\n",
+                userName, hours);
+    }
 
+    public static void promptUser() {
+        System.out.println("Hello and welcome!");
+        System.out.print("What is your name Fellow Traveler? ");
+        userName = scan.nextLine();
+        System.out.print("How many hours have you worked? ");
+        // Update the hours variable
+        hours = scan.nextDouble();
+        System.out.print("Alright and how much would you like to get paid for your work " + userName + "? ");
+        // Update the hourlyPay variable
+        hourlyPay = scan.nextDouble();
+    }
+
+    public static void displayMessage(double hours, double hourlyPay) {
+        grossPay = hours * hourlyPay;
+        System.out.printf("Impressive work, %s! Your earnings for this week are $%.2f.\n", userName, grossPay);
+        System.out.printf("Congratulations, %s! You've worked %.2f hours this week! Your commitment is inspiring! :)\n",
+                userName, hours);
+    }
+
+    public static void payBreakDown() {
+        System.out.println("~ Payroll Summary ~");
+        System.out.println("Name: " + userName);
+        System.out.printf("Hours Worked: %.2f\n", hours);
+        System.out.printf("Pay Rate: $%.2f\n", hourlyPay);
+        System.out.printf("Total Pay: $%.2f\n", grossPay);
+        System.out.println("Don't go spending that money all in one place now " + userName + " :) !");
+    }
 }
