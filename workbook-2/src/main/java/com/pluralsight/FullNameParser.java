1027331a666f9a5e7cadb846cdced6fc327e452a
Noel Guillen <noelblas274@gmail.com>
2024-04-12 15:52:29 -0400

    FullNameParser with error handle complete

--- workbook-2/src/main/java/com/pluralsight/FullNameParser.java
+++ workbook-2/src/main/java/com/pluralsight/FullNameParser.java
@@ -1,2 +1,44 @@
-package com.pluralsight;public class FullNameParser {
+package  com.pluralsight;
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class FullNameParser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String[] arrayOfName;
+        System.out.print("Please enter your full name: ");
+        name = scanner.nextLine().trim();
+        arrayOfName = name.split(Pattern.quote(" "));
+
+        // If the length is not within the expected range, prompt the user to enter the name again
+        while (arrayOfName.length < 2 || arrayOfName.length > 3) {
+            System.out.println("Full name should contain 2 or 3 parts.");
+            System.out.print("Please enter your full name: ");
+            name = scanner.nextLine().trim();
+            arrayOfName = name.split(Pattern.quote(" "));
+        }
+
+        String displayFirstName = capitalizeFirstLetter(arrayOfName[0]);
+        String displayMiddleName = arrayOfName.length == 3 ? capitalizeFirstLetter(arrayOfName[1]).charAt(0) + "." : "(None)";
+        String displayLastName = capitalizeFirstLetter(arrayOfName[arrayOfName.length - 1]);
+        //since its zero based the length of [3] is out of bounds. so we minus one to always get the last index. EX. last name
+
+        System.out.println("First Name: " + displayFirstName);
+        System.out.println("Middle Name: " + displayMiddleName);
+        System.out.println("Last Name: " + displayLastName);
+    }
+
+
+    public static String capitalizeFirstLetter(String str) {
+        if (str.isEmpty()) {
+            return str;
+        }
+        //grab the first letter and uppercase it
+        //then grab the other half of word and add it
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
 }
--- workbook-2/src/main/java/com/pluralsight/StringRefs.java
+++ workbook-2/src/main/java/com/pluralsight/StringRefs.java
@@ -1,2 +1,34 @@
-package com.pluralsight;public class StringRefs {
+package com.pluralsight;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class StringRefs {
+    public static void main(String[] args) {
+        String welcome = "Big Tex says \"Howdy\"!";
+        String welcome2 = "Let's get\r this started!";
+
+        String name = "    Noel Guilen    ";
+//        System.out.println(name.trim());
+
+        String discountCode = "FAIR-15OFF";
+//        System.out.println(discountCode.endsWith("F"));
+//        System.out.println(discountCode.startsWith("F"));
+        String trackingCode = "USA-12981-Y-22";
+        char hasShipped = trackingCode.charAt(10);
+        System.out.println(hasShipped);
+        String productCode = "ACME-12213";
+        int dashPosition = productCode.indexOf("E");
+//        System.out.println(dashPosition);
+       String vendor = productCode.substring(0, 3);
+//        System.out.println(vendor);
+       String code = productCode.substring(5,10);
+//        System.out.println(code);
+
+        String input = "Dallas|Ft. Worth|Austin";
+        String[] cities = input.split("\\|");
+        System.out.println(Arrays.toString(cities));
+        String[] cities2 = input.split(Pattern.quote("|"));
+        System.out.println(Arrays.toString(cities2));
+}
 }
