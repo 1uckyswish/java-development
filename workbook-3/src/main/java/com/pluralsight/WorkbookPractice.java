95ebbef5f889bdd244f361b775a52bef3782678c
Noel Guillen <noelblas274@gmail.com>
2024-04-15 19:37:17 -0400

    added file for test on FILE IO

--- workbook-3/src/main/java/com/pluralsight/WorkbookPractice.java
+++ workbook-3/src/main/java/com/pluralsight/WorkbookPractice.java
@@ -1,8 +1,25 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class WorkbookPractice {
     public static void main(String[] args) {
-        System.out.println("Hello workbook 3");
-        System.out.println("Test commit");
+//        String myStr = "I love cats. Cats are very easy to love. Cats are very popular.";
+//        String regex = "(?i)cats";
+//        System.out.println(myStr.replaceAll(regex, "dogs"));
+       try{
+           FileInputStream fis = new FileInputStream("poem.txt");
+           Scanner scanner = new Scanner(fis);
+           String input;
+            while(scanner.hasNextLine()){
+                input = scanner.nextLine();
+                System.out.println(input);
+            }
+           scanner.close();
+       }catch (Exception e){
+        // display stack trace if there was an error
+           e.printStackTrace();
+       }
     }
 }
