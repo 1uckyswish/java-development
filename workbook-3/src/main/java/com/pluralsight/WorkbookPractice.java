a972bb5be90aff02dd0b27bce66e1f17e4d6cc3c
Noel Guillen <noelblas274@gmail.com>
2024-04-16 09:05:45 -0400

    new writer added

--- workbook-3/src/main/java/com/pluralsight/WorkbookPractice.java
+++ workbook-3/src/main/java/com/pluralsight/WorkbookPractice.java
@@ -22,23 +22,46 @@ public class WorkbookPractice {
 //           e.printStackTrace();
 //       }
 
-        try
-        {
-// create a FileReader object connected to the File
-            FileReader fileReader = new FileReader("poem.txt");
-// create a BufferedReader to manage input stream
-            BufferedReader bufReader = new BufferedReader(fileReader);
-            String input;
-// read until there is no more data
-            while((input = bufReader.readLine()) != null) {
-                System.out.println(input);
+//        try
+//        {
+//// create a FileReader object connected to the File
+//            FileReader fileReader = new FileReader("poem.txt");
+//// create a BufferedReader to manage input stream
+//            BufferedReader bufReader = new BufferedReader(fileReader);
+//            String input;
+//// read until there is no more data
+//            while((input = bufReader.readLine()) != null) {
+//                System.out.println(input);
+//            }
+//// close the stream and release the resources
+//            bufReader.close();
+//        }
+//        catch(IOException e) {
+//// display stack trace if there was an error
+//            e.printStackTrace();
+//        }
+
+//        try{
+//        FileWriter writer = new FileWriter("skills.txt");
+//        writer.write("Skills: \n");
+//        writer.write("Git, HTML, CSS, Boostrap\n");
+//        writer.write("JAVA");
+//        writer.close();
+//        }catch(Exception e){
+//            System.out.println(e.getMessage());
+//        }
+
+        try{
+            FileWriter fileWriter = new FileWriter("skills.txt");
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            String text;
+            for (int i = 1; i <= 10; i++) {
+                text = String.format("Counting %d\n", i);
+                bufWriter.write(text);
             }
-// close the stream and release the resources
-            bufReader.close();
-        }
-        catch(IOException e) {
-// display stack trace if there was an error
-            e.printStackTrace();
+            bufWriter.close();
+        }catch(Exception e){
+            System.out.println(e.getMessage());
         }
     }
 }
