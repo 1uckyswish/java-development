5f8fd43e94db82c1415dffb9539c4309fc319595
Noel Guillen <noelblas274@gmail.com>
2024-04-15 20:12:57 -0400

    BedtimeStories excercise done

--- workbook-3/Mod01/Mod01/src/main/java/com/pluralsight/BedtimeStories.java
+++ workbook-3/Mod01/Mod01/src/main/java/com/pluralsight/BedtimeStories.java
@@ -1,7 +1,42 @@
 package com.pluralsight;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class BedtimeStories {
     public static void main(String[] args) {
-        System.out.println("Goodnight");
+        startBedTime();
+    }
+
+    public static void startBedTime() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please choose a bedtime story:");
+        System.out.println("1. goldilocks.txt");
+        System.out.println("2. hansel_and_gretel.txt");
+        System.out.println("3. mary_had_a_little_lamb.txt");
+        System.out.print("Enter the name of your choice: ");
+        String bookName = scanner.nextLine();
+        System.out.println();
+        readBook(bookName);
+        scanner.close();
+    }
+
+    public static void readBook(String bookName) {
+        try{
+            FileInputStream file = new FileInputStream(bookName);
+            Scanner scanner = new Scanner(file);
+            String fileLine;
+            int numberOfLine = 0;
+            while(scanner.hasNextLine()){
+                fileLine = scanner.nextLine();
+                numberOfLine++;
+                Thread.sleep(1000);
+                System.out.printf("%d.   %s\n", numberOfLine, fileLine);
+            }
+            scanner.close();
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+            startBedTime();
+        }
     }
 }
