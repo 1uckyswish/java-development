6bc0b1c8e03f429a7aafea4628c4304a050c97d2
Noel Guillen <noelblas274@gmail.com>
2024-04-22 11:50:45 -0400

    Exercise creating classes done

---
+++ workbook-4/workbook-4/src/main/java/com/pluralsight/HotelOperations/Employee.java
@@ -0,0 +1,70 @@
+package com.pluralsight.HotelOperations;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private String department;
+    private double payRate;
+    private double hoursWorked;
+
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public double getTotalpay() {
+        return hoursWorked * payRate;
+    }
+    public double getRegularHours() {
+        // Regular hours are either 40 or less if the total hours worked are less than 40
+        return Math.min(hoursWorked, 40);
+    }
+
+    public double getOvertimeHours() {
+        // Overtime hours are the hours worked beyond 40
+        return Math.max(hoursWorked - 40, 0); // Return 0 if hours worked are 40 or less
+    }
+}
---
+++ workbook-4/workbook-4/src/main/java/com/pluralsight/HotelOperations/Reservation.java
@@ -0,0 +1,58 @@
+package com.pluralsight.HotelOperations;
+
+public class Reservation {
+    private String roomType;
+    private double pricePerNight;
+    private int numberOfNights;
+    private boolean isWeekend;
+
+    public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
+        this.roomType = roomType;
+        this.numberOfNights = numberOfNights;
+        this.isWeekend = isWeekend;
+        // Set price per night based on room type
+        if (roomType.equals("king")) {
+            this.pricePerNight = 139.00;
+        } else if (roomType.equals("double")) {
+            this.pricePerNight = 124.00;
+        }
+        // Adjust price if it's a weekend stay
+        if (isWeekend) {
+            this.pricePerNight *= 1.1; // Increase price by 10%
+        }
+    }
+
+    // Getters and Setters
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public double getPricePerNight() {
+        return pricePerNight;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return isWeekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        isWeekend = weekend;
+    }
+
+    // Calculate total reservation cost
+    public double getReservationTotal() {
+        return pricePerNight * numberOfNights;
+    }
+}
