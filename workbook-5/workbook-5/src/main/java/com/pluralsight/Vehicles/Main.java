ff26f4c6fbfbb6f2988d9bdf9666fe842bd48203
Noel Guillen <noelblas274@gmail.com>
2024-04-25 11:10:25 -0400

    using object methods for excerise

--- workbook-5/workbook-5/src/main/java/com/pluralsight/Vehicles/Main.java
+++ workbook-5/workbook-5/src/main/java/com/pluralsight/Vehicles/Main.java
@@ -3,13 +3,18 @@ package com.pluralsight.Vehicles;
 public class Main {
     public static void main(String[] args) {
         Car bmw = new Car("Green", 4, 4, 100);
-        Moped slowRide = new Moped("Red", 1, 1, 10);
-        HoverCraft martyMcfly = new HoverCraft("Yellow", 1, 0, 300);
-        SemiTruck trailer = new SemiTruck("Blue", 2, 8, 1000);
-
         System.out.println(bmw);
+        Moped slowRide = new Moped("Red", 1, 1, 10);
         System.out.println(slowRide);
+        HoverCraft martyMcfly = new HoverCraft("Yellow", 1, 0, 300);
         System.out.println(martyMcfly);
+        SemiTruck trailer = new SemiTruck("Blue", 2, 8, 1000);
         System.out.println(trailer);
+
+        System.out.println(bmw instanceof Vehicle);
+        System.out.println(slowRide.getClass());
+
+        String name = "noel";
+        System.out.println(name.hashCode());
     }
 }
