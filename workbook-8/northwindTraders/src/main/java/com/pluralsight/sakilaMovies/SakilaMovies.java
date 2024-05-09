9bf74b72bad5c5ac80471653d9c1b24bdbc7d30f
Noel Guillen <noelblas274@gmail.com>
2024-05-09 14:08:01 -0400

    Join excerise from sakila movies

--- workbook-8/northwindTraders/src/main/java/com/pluralsight/sakilaMovies/SakilaMovies.java
+++ workbook-8/northwindTraders/src/main/java/com/pluralsight/sakilaMovies/SakilaMovies.java
@@ -3,8 +3,10 @@ package com.pluralsight.sakilaMovies;
 import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.Scanner;
 
 public class SakilaMovies {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         if (args.length != 2) {
             System.out.println("Application needs two arguments to run: java com.hca.jdbc.UsingDriverManager <username> <password>");
@@ -21,12 +23,74 @@ public class SakilaMovies {
     }
 
     public static void displayMoviesAndActor(BasicDataSource dataSource){
-        try(Connection connection = dataSource.getConnection()
-
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT first_name, last_name FROM actor;");
         ){
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                System.out.println("\t\tActor Names");
+                System.out.printf("%-12s | %-12s\n", "First Name", "Last Name"); // Header with labels
+                System.out.println("--------------------------------");
+                while(resultSet.next()){
+                    System.out.printf(
+                            "%-12s | %-12s\n", // Format string with labels
+                            resultSet.getString(1), resultSet.getString(2)
+                    );
+                }
 
+                System.out.println("Please Enter the Actor info to search movies they are in!");
+                System.out.print("Actor First Name: ");
+                String firstName = scanner.nextLine().trim();
+                System.out.print("Actor Last Name: ");
+                String lastName = scanner.nextLine().trim();
+                displayMoviesByQuery(dataSource, firstName, lastName);
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+
+    public static void displayMoviesByQuery(BasicDataSource dataSource, String firstName, String lastName){
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT F.title FROM sakila.actor AS A\n" +
+                    "JOIN\n" +
+                    "\tfilm_actor AS FA\n" +
+                    "ON\n" +
+                    "\tA.actor_id = FA.actor_id\n" +
+                    "JOIN \n" +
+                    "\tfilm AS F\n" +
+                    "ON\n" +
+                    "\tFA.film_id = F.film_id\n" +
+                    "WHERE A.first_name = ? AND A.last_name = ?;");
+        ){
+            preparedStatement.setString(1, firstName);
+            preparedStatement.setString(2, lastName);
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                System.out.println("Movies Actor is in");
+                System.out.printf("%-12s\n", "Movie Title"); // Header with labels
+                System.out.println("--------------------------------");
+
+                if(resultSet.next()){
+                    System.out.println("~~~Your matches are~~~\n");
+                    do{
+                        System.out.printf(
+                                "%-12s\n", // Format string with labels
+                                resultSet.getString(1));
+                    }while (resultSet.next());
+                }
+                else{
+                    System.out.println("No Matches Found");
+                }
+
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
 }
