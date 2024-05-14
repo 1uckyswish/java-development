32bbde7dad263bb10a2c52a6afb9e10d145d8915
Noel Guillen <noelblas274@gmail.com>
2024-05-14 16:01:11 -0400

    added JDBC spring excerise

---
+++ workbook-9/NorthwindTradersSpringBootJDBC/src/main/java/com/pluralsight/NorthwindTradersSpringBootJDBC/models/Product.java
@@ -0,0 +1,57 @@
+package com.pluralsight.NorthwindTradersSpringBootJDBC.models;
+
+public class Product {
+    private int productId;
+    private String name;
+    private int category;
+    private double price;
+
+    public Product(int productId, String name, int category, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productId=" + productId +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
